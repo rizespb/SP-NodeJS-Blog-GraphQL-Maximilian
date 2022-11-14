763577a79d8a692237efc2818cd3ae86254603a4
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-11-14 13:41:16 +0300

    Удаление socket.io с прошлого проекта

--- frontend/src/pages/Feed/Feed.js
+++ frontend/src/pages/Feed/Feed.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react'
-import openSocket from 'socket.io-client'
 
 import Post from '../../components/Feed/Post/Post'
 import Button from '../../components/Button/Button'
@@ -36,55 +35,6 @@ class Feed extends Component {
       .catch(this.catchError)
 
     this.loadPosts()
-
-    // Устанавливаем webSocket-соединение
-    const socket = openSocket('http://localhost:8080')
-
-    // Устанавливаем слушателя на сокет
-    // 'posts' - имя события, задаваемое на бэке
-    // data - информация с сервера (структура этого объекта произвольная, задается на бэке)
-    socket.on('posts', (data) => {
-      if (data.action === 'create') {
-        this.addPost(data.post)
-      } else if (data.action === 'update') {
-        this.updatePost(data.post)
-      } else if (data.action === 'delete') {
-        console.log('Socket', data)
-        this.loadPosts()
-      }
-    })
-  }
-
-  addPost = (post) => {
-    this.setState((prevState) => {
-      const updatedPosts = [...prevState.posts]
-
-      if (prevState.postPage === 1) {
-        if (prevState.posts.length >= 2) {
-          updatedPosts.pop()
-        }
-
-        updatedPosts.unshift(post)
-      }
-
-      return {
-        posts: updatedPosts,
-        totalPosts: prevState.totalPosts + 1,
-      }
-    })
-  }
-
-  updatePost = (post) => {
-    this.setState((prevState) => {
-      const updatedPosts = [...prevState.posts]
-      const updatedPostIndex = updatedPosts.findIndex((p) => p._id === post._id)
-      if (updatedPostIndex > -1) {
-        updatedPosts[updatedPostIndex] = post
-      }
-      return {
-        posts: updatedPosts,
-      }
-    })
   }
 
   loadPosts = (direction) => {
