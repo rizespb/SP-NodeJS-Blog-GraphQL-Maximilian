223f3a62275a4d21a278719b69eab996d6294087
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-11-16 10:01:24 +0300

    Регистрация на клиенте с помощью GraphQL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -106,29 +106,41 @@ class App extends Component {
   signupHandler = (event, authData) => {
     event.preventDefault()
     this.setState({ authLoading: true })
-    fetch('http://localhost:8080/auth/signup', {
-      method: 'PUT',
+
+    const graphqlQuery = {
+      // Вначале передаем параметры в резолвер createUser
+      // Потом указываем, какие именно данные из тех, что возвращает резолвер, мы хотим получить
+      query: `
+        mutation {
+          createUser(userInput:{email: "${authData.signupForm.email.value}", name: "${authData.signupForm.name.value}", password: "${authData.signupForm.password.value}"}) {
+            _id
+            email
+          }
+        }
+      `,
+    }
+
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        email: authData.signupForm.email.value,
-        password: authData.signupForm.password.value,
-        name: authData.signupForm.name.value,
-      }),
+      body: JSON.stringify(graphqlQuery),
     })
       .then((res) => {
-        if (res.status === 422) {
-          throw new Error("Validation failed. Make sure the email address isn't used yet!")
-        }
-        if (res.status !== 200 && res.status !== 201) {
-          console.log('Error!')
-          throw new Error('Creating a user failed!')
-        }
         return res.json()
       })
       .then((resData) => {
         console.log(resData)
+
+        if (resData.erorrs && resData.erorrs[0].status === 422) {
+          throw new Error("Validation failed. Make sure the email address isn't used yet!")
+        }
+
+        if (resData.errors) {
+          throw new Error('User creation failed')
+        }
+
         this.setState({ isAuth: false, authLoading: false })
         this.props.history.replace('/')
       })
