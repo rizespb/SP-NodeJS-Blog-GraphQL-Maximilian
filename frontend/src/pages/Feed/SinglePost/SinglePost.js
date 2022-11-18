63698aa2e3ed670cf0528a7e078ffba3ed0fc56c
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-11-18 12:04:17 +0300

    Получение одного поста на клиенте

--- frontend/src/pages/Feed/SinglePost/SinglePost.js
+++ frontend/src/pages/Feed/SinglePost/SinglePost.js
@@ -15,25 +15,44 @@ class SinglePost extends Component {
   componentDidMount() {
     const postId = this.props.match.params.postId
 
-    fetch('http://localhost:8080/feed/post/' + postId, {
+    const qraphqlQuery = {
+      query: `{
+        post(id: "${postId}") {
+          title
+          content
+          imageUrl
+          creator {
+            name
+          }
+          createdAt
+        }
+      }
+      `,
+    }
+
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
       headers: {
-        // Использование Bearer - это договоренность. Не обязательно
+        // Использование Bearer - это договоренность. Можно другое имя
         Authorization: 'Bearer ' + this.props.token,
+        'Content-type': 'application/json',
       },
+      body: JSON.stringify(qraphqlQuery),
     })
       .then((res) => {
-        if (res.status !== 200) {
-          throw new Error('Failed to fetch status')
-        }
         return res.json()
       })
       .then((resData) => {
+        if (resData.errors) {
+          throw new Error('Fetching post failed at SinglePost')
+        }
+
         this.setState({
-          title: resData.post.title,
-          author: resData.post.creator.name,
-          image: 'http://localhost:8080/' + resData.post.imageUrl,
-          date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
-          content: resData.post.content,
+          title: resData.data.post.title,
+          author: resData.data.post.creator.name,
+          image: 'http://localhost:8080/' + resData.data.post.imageUrl,
+          date: new Date(resData.data.post.createdAt).toLocaleDateString('en-US'),
+          content: resData.data.post.content,
         })
       })
       .catch((err) => {
