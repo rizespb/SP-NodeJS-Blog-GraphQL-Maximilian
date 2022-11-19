708c90f204c4931f63369956ba3ba13932c7f9c9
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2022-11-19 13:56:34 +0300

    Резолвер updateStatus - Обновление статуса пользователя на сервере

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -355,4 +355,29 @@ module.exports = {
 
     return { ...user._doc, _id: user._id.toString() }
   },
+
+  // Обновление статуса пользователя
+  updateStatus: async function ({ status }, req) {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated!')
+      error.code = 401
+
+      // Ошибкку обработает GraphQL в formatError в app.js
+      throw error
+    }
+
+    const user = await User.findById(req.userId)
+
+    if (!user) {
+      const error = new Error('No user found!')
+      error.code = 404
+
+      throw error
+    }
+
+    user.status = status
+    await user.save()
+
+    return { ...user._doc, _id: user._id.toString() }
+  },
 }
